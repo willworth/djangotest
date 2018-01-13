d0a8e497a86b0200fc02c0e16a4914453ce699c6
Will Worth <will.worth@gmail.com>
2018-01-13 17:18:56 +0100

    admin layout

--- tictactoe/gameplay/admin.py
+++ tictactoe/gameplay/admin.py
@@ -6,6 +6,8 @@ from .models import Game, Move
 @admin.register(Game)
 class GameAdmin(admin.ModelAdmin):
     list_display = ('id', 'first_player', 'second_player', 'status')
+    list_editable = ('status',)
+    #comma above is to indicate it's a tuple
 
 
 admin.site.register(Move)
