57caec1f48c7edcfcc4d288b1f6315c1df3ec996
Will Worth <will.worth@gmail.com>
2018-01-13 15:04:33 +0100

    add admin layout

--- tictactoe/gameplay/admin.py
+++ tictactoe/gameplay/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from .models import Game, Move
 
-admin.site.register(Game)
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_player', 'second_player', 'status')
+
+
 admin.site.register(Move)
