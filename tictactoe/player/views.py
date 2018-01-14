eb100c689ffb0bc9e103ee3e0d94cb0a4e04f7dc
Will Worth <will.worth@gmail.com>
2018-01-14 09:48:55 +0100

    retry git error with different msg!

--- tictactoe/player/views.py
+++ tictactoe/player/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
+from gameplay.models import Game
+
 def home(request):
-    return render(request, 'player/home.html')
+    return render(request, 'player/home.html',
+                  {'ngames': Game.objects.count()})
