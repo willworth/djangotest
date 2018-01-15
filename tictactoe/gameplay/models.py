b8dd8e807992c09848b88a3471f6edc3f985da29
Will Worth <will.worth@gmail.com>
2018-01-15 14:25:26 +0100

    add initializr front end

--- tictactoe/gameplay/models.py
+++ tictactoe/gameplay/models.py
@@ -11,12 +11,13 @@ GAME_STATUS_CHOICES = (
 )
 
 
-class GameQuerySet(models.QuerySet):
+class GamesQuerySet(models.QuerySet):
     def games_for_user(self, user):
 
         return self.filter(
             Q(first_player=user) | Q(second_player=user)
         )
+
     def active(self):
         return self.filter(
             Q(status='F') | Q(status='S')
@@ -33,7 +34,7 @@ class Game(models.Model):
     last_active = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=1, default='F', choices=GAME_STATUS_CHOICES)
 
-    objects = GameQuerySet.as_manager()
+    objects = GamesQuerySet.as_manager()
     #the following is what gives the player names in admin game display
     def __str__(self):
         return "{0} vs {1}".format(
